0b4de8253da25359d04619dba9d44b6f2cf8aba3
Adriano Ceccarelli <aceccarelli@gmail.com>
2016-10-05 08:58:34 -0300

    Update form.php

--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Credentials:true");
 header("Access-Control-Allow-Methods:GET, POST, OPTIONS");
-header("Access-Control-Allow-Origin:https:sisloc1464960274.zendesk.com");
+header("Access-Control-Allow-Origin:https://sisloc1464960274.zendesk.com");
 header("Access-Control-Max-Age:3600");
 header("Content-Type:application/json;charset=utf-8");
 json_encode(print_R($_POST));
